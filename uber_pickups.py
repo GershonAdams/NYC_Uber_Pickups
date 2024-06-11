efb18016da57907356df76504c3cb85b4a9f111a
GershonAdams <42353178+GershonAdams@users.noreply.github.com>
2024-06-11 10:10:25 +0000

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -30,11 +30,11 @@ data = load_data(10000)
 load_data_state.text("Done! (using st.cache_data)")
 
 if st.checkbox('Show raw data?'):
-    st.subheader("Raw data")
+    st.subheader(":orange[Raw data]", divider=True)
     st.write(data)
 
 # Draw a histogram
-st.subheader("Number of pickups by hour")
+st.subheader(":orange[Number of pickups by hour]")
 
 # Use numpy to generate a histogram than breaks down pickup times binned by hour
 hist_values = np.histogram(data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
@@ -50,6 +50,6 @@ st.bar_chart(hist_values)   # Draws the histogram
 hour_to_filter = hour_to_filter = st.slider('hour', 0, 23, 17)  # min: 0h, max: 23h, default: 17h
 
 filtered_data = data[data[DATE_COLUMN].dt.hour == hour_to_filter]
-st.subheader(f'Map of all pickups at {hour_to_filter}:00')
+st.subheader(f':orange[Map of all pickups at {hour_to_filter}:00]')
 st.map(filtered_data)
 
